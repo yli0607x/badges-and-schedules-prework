b6601d39b49f5f7b93c0d893af5118a1f7ef4505
Yu Li <yli0607x@gmail.com>
2018-10-05 01:15:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,25 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each { |name| badge_maker(name)}
-  return badges
+  attendees.collect do |name| 
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+ room = 0
+ speakers.array do |name|
+  room +=1
+ "Hello, #{name}! You'll be assigned to room #{room}!"
+ end
+ end
+ 
+def printer(array)
+  batch_badge_creator(array).each do |val|
+    puts val
+  end
+
+  assign_rooms(array). each do |val|
+    puts val
+  end
 end
